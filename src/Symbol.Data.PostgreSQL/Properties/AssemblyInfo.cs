385edb4ce724e97243e3824901ae2dabaf35cdc0
symbolspace <symbolspace@outlook.com>
2019-06-26 04:57:51 +0800

    release flag

--- src/Symbol.Data.PostgreSQL/Properties/AssemblyInfo.cs
+++ src/Symbol.Data.PostgreSQL/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("symbolspace")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© symbolspace All rights reserved.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Please see https://github.com/symbolspace/Symbol.Data/wiki/Home for more informat" +
     "ion.")]
